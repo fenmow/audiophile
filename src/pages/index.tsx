e47c4250c0d37c275fd80a41616c47a678b023b0
fenmow <lucas170105@outlook.com>
2023-12-14 10:58:32 -0300

    Removed all API calls

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,12 @@
-import AudioGear from "@/components/AudioGear/AudioGear";
-import CategoryRow from "@/components/CategoryRow/CategoryRow";
 import Header from "@/components/Header/Header";
-import HomeMainCard from "@/components/HomeMainCard/HomeMainCard";
-import ProductLinks from "@/components/ProductLinks/ProductLinks";
-import { ProductType, fetchProducts } from "@/services/products";
-import { GetStaticProps, NextPage } from "next";
+import { NextPage } from "next";
 
-export const getStaticProps: GetStaticProps = async () => {
-  const products: ProductType[] = await fetchProducts()
-  return { props: { products } } 
-}
+const Home: NextPage = () => {
 
-const Home: NextPage = (props: {
-  products?: ProductType[]
-}) => {
-
-  const markTwo = props.products?.find(product => product.slug === 'xx99-mark-two-headphones')
-  const zx9 = props.products?.find(product => product.slug === 'zx9-speaker')
-  const zx7 = props.products?.find(product => product.slug === 'zx7-speaker')
-  const yx1 = props.products?.find(product => product.slug === 'yx1-earphones')
 
   return (
     <>
       <Header />
-      <HomeMainCard productName={markTwo?.name} slug={markTwo?.slug} />
-      <CategoryRow />
-      <ProductLinks firstProduct={zx9} secondProduct={zx7} thirdProduct={yx1} />
-      <AudioGear />
     </>
   )
 }

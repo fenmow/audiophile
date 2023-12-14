192855f1f0d28c73367782bbbbf9f1bfd1689069
fenmow <lucas170105@outlook.com>
2023-12-14 11:04:27 -0300

    Readded API calls

---
+++ src/components/ProductLinks/ProductLinks.tsx
@@ -0,0 +1,61 @@
+import Image from "next/image"
+import Style from "./ProductLinks.module.scss"
+import SpeakerImage from "../../../public/Home/image-speaker-zx9.png"
+import Zx7Image from "../../../public/Home/image-speaker-zx7.jpg"
+import EarphoneImage from "public/Home/image-earphones-yx1.jpg"
+import { ProductType } from "@/services/products"
+import PatternCircles from "../../../public/Home/pattern-circles.svg"
+import Link from "next/link"
+
+type ProductLinkProps = {
+  firstProduct: ProductType | undefined
+  secondProduct: ProductType | undefined
+  thirdProduct: ProductType | undefined
+}
+
+const ProductLinks: React.FC<ProductLinkProps> = (props: {
+  firstProduct?: ProductType | undefined
+  secondProduct?: ProductType | undefined
+  thirdProduct?: ProductType | undefined
+}) => {
+  return (
+    <section className={Style.product_links}>
+      <div className={Style.product_links_container}>
+        <div className={`${Style.product_row} ${Style.first_product}`} style={{ backgroundImage: `url(${PatternCircles.src})` }}>
+          <div className={Style.image_column}>
+            <Image className={Style.image} src={SpeakerImage.src} height={450} width={350} alt="ZX9 SPEAKER"></Image>
+          </div>
+
+          <div className={Style.text_content}>
+            <h1>{props.firstProduct?.name}</h1>
+            <p>Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.</p>
+            <Link href={`/products/${props.firstProduct?.slug.toString()}`}>
+              <button>see product</button>
+            </Link>
+          </div>
+        </div>
+        <div className={`${Style.product_row} ${Style.second_product}`} style={{ backgroundImage: `url(${Zx7Image.src})` }}>
+          <div className={Style.text_content}>
+            <h1>{props.secondProduct?.name}</h1>
+            <Link href={`/products/${props.secondProduct?.slug.toString()}`}>
+                <button>see product</button>
+            </Link>
+          </div>
+        </div>
+        <div className={`${Style.product_row} ${Style.third_product}`}>
+          <div className={Style.image_column}>
+            <Image className={Style.image} src={EarphoneImage.src} alt="earphone" height={300} width={500}></Image>
+          </div>
+          <div className={Style.text_content}>
+            <h1>{props.thirdProduct?.name}</h1>
+            <Link href={`/products/${props.secondProduct?.slug.toString()}`}>
+                <button>see product</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default ProductLinks
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,32 @@
+import AudioGear from "@/components/AudioGear/AudioGear";
+import CategoryRow from "@/components/CategoryRow/CategoryRow";
 import Header from "@/components/Header/Header";
-import { NextPage } from "next";
+import HomeMainCard from "@/components/HomeMainCard/HomeMainCard";
+import ProductLinks from "@/components/ProductLinks/ProductLinks";
+import { ProductType, fetchProducts } from "@/services/products";
+import { GetStaticProps, NextPage } from "next";
 
-const Home: NextPage = () => {
+export const getStaticProps: GetStaticProps = async () => {
+  const products: ProductType[] = await fetchProducts()
+  return { props: { products } } 
+}
 
+const Home: NextPage = (props: {
+  products?: ProductType[]
+}) => {
+
+  const markTwo = props.products?.find(product => product.slug === 'xx99-mark-two-headphones')
+  const zx9 = props.products?.find(product => product.slug === 'zx9-speaker')
+  const zx7 = props.products?.find(product => product.slug === 'zx7-speaker')
+  const yx1 = props.products?.find(product => product.slug === 'yx1-earphones')
 
   return (
     <>
       <Header />
+      <HomeMainCard productName={markTwo?.name} slug={markTwo?.slug} />
+      <CategoryRow />
+      <ProductLinks firstProduct={zx9} secondProduct={zx7} thirdProduct={yx1} />
+      <AudioGear />
     </>
   )
 }
